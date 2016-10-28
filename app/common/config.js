ea5086cfcaa02c69daabad9cffcbad1a5fc1f6c5
zhangdong <892498818@qq.com>
2016-10-28 04:28:07 -0700

    增加获取验证码和登录接口

--- app/common/config.js
+++ app/common/config.js
@@ -12,6 +12,8 @@ module.exports = {
         base:'http://rap.taobao.org/mockjs/9013/',//主入口
         creations:'manlist?',//主页视频列表地址
         comment:'detail/comment?',//评论信息地址
-        sbumitComment:'detail/submitComment?'//提交评论地址
+        sbumitComment:'detail/submitComment?',//提交评论地址
+        verificationCode:'login/verificationCode?',//获取验证码接口
+        login:'login/phonelogin?'//用户登录接口
     }
 }
